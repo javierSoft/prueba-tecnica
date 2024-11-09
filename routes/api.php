57e410cbb6b72a6d51877912868b40919f682856
javierSoft <javierg.software@gmail.com>
2024-11-09 18:06:24 -0500

    JA Se crean las rutas de productos

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/productos', function () {
+    return 'Ver productos';
+});
+
+Route::get('/productos/{id}', function () {
+    return 'Ver producto';
+});
+
+Route::post('/productos', function () {
+    return 'Crear productos';
+});
+
+Route::put('/productos/{id}', function () {
+    return 'Actualizar productos';
+});
+
+Route::delete('/productos/{id}', function () {
+    return 'Eliminar productos';
+});
